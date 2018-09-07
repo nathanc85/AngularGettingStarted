bf4ae303d019228852d106965d2e6d9be781e0ed
Nathan Costea <nathanC@knowclassic.com>
2018-09-07 16:18:44 -0400

    Manually create the AppComponent

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+    <h1>{{pageTitle}}</h1>
+    <!-- <h2>{{randomWord()}}</h2> -->
+    <div>My First Component</div>
+  </div>`
 })
 export class AppComponent {
-  title = 'Angular: Getting Started Now';
-  weekDay: string = 'Friday';
+  pageTitle: string = "Acme Product Management";
+
+  randomWord():string {
+    let words = ["house", "table", "light", "bed"];
+    return words[Math.floor(Math.random() * words.length)];
+  }
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started Now';
+//   weekDay: string = 'Friday';
+// }
